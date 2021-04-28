4b204d59578f34c6dbbcd7e676c1b833b577d26b
DoLim <limdongyoung0@gmail.com>
2021-04-28 21:10:16 +0900

    feat: changed react cdn to production

--- src/extension.ts
+++ src/extension.ts
@@ -41,8 +41,8 @@ function getWebviewContent(bundledUri: vscode.Uri) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cat Coding</title>
-    <script crossorigin src="https://unpkg.com/react@17/umd/react.development.js"></script>
-    <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.development.js"></script>
+    <script crossorigin src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
+    <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>
 </head>
 <body>
     <div id="root"></div>
