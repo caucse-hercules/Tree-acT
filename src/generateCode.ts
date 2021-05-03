6fe824b9af605b02af161723ce0a0fd9ae4b6778
DoLim <limdongyoung0@gmail.com>
2021-05-04 01:18:00 +0900

    fix: npx execution fix

--- src/generateCode.ts
+++ src/generateCode.ts
@@ -1,16 +1,19 @@
 import { sampleGenerateMessage } from "./common/sampleData";
-import { runInTerminal } from 'run-in-terminal';
-import fs from 'fs';
-import path from 'path';
-import * as vscode from 'vscode';
-import waitUntil from "async-wait-until";
+import fs from "fs";
+import path from "path";
+import * as vscode from "vscode";
+import * as child_process from "child_process";
 
-const folder: string = vscode.workspace.rootPath || `C:\\Users\\"문법식"\\Desktop\\test3`;
-const generateComponentPath = path.join(folder, <string>sampleGenerateMessage.directory, 'src/components');
-const str1 = 'const ';
-const str2 = ' = () => { \n return <div>';
-const str3 = '</div>; \n};\n\n';
-
+const folder: string =
+  vscode.workspace.rootPath || `C:\\Users\\"문법식"\\Desktop\\test3`;
+const generateComponentPath = path.join(
+  folder,
+  <string>sampleGenerateMessage.directory,
+  "src/components"
+);
+const str1 = "const ";
+const str2 = " = () => { \n return <div>";
+const str3 = "</div>; \n};\n\n";
 
 const makeFolder = () => {
   if (!fs.existsSync(generateComponentPath)) {
@@ -30,23 +33,44 @@ function FileSystem(folder: string, str: string) {
   }
 }
 function dfs(component: any) {
-  FileSystem(generateComponentPath + '/' + component.name + '.js', 'import React from \'react\';\n');
+  FileSystem(
+    generateComponentPath + "/" + component.name + ".js",
+    "import React from 'react';\n"
+  );
 
   for (const i in component.children) {
     const next: any = component.children[i];
     console.log(next.name);
-    FileSystem(generateComponentPath + '/' + component.name + '.js', 'import ' + next.name + ' from \'' + './' + next.name + '\';\n');
+    FileSystem(
+      generateComponentPath + "/" + component.name + ".js",
+      "import " + next.name + " from '" + "./" + next.name + "';\n"
+    );
     dfs(next);
   }
 
-  FileSystem(generateComponentPath + '/' + component.name + '.js', '\n' + str1 + component.name + str2 + component.name + str3);
-  FileSystem(generateComponentPath + '/' + component.name + '.js', 'export default ' + component.name + ';\n');
+  FileSystem(
+    generateComponentPath + "/" + component.name + ".js",
+    "\n" + str1 + component.name + str2 + component.name + str3
+  );
+  FileSystem(
+    generateComponentPath + "/" + component.name + ".js",
+    "export default " + component.name + ";\n"
+  );
 }
 
-export async function run() {
-  const result = await runInTerminal(`cd ${folder} && npx create-react-app ${sampleGenerateMessage.directory}`);
-  await waitUntil(() => result.killed);
-  makeFolder();
-  dfs(sampleGenerateMessage.data);
-
+export function run() {
+  console.log("Started!");
+  const child = child_process.exec(
+    `cd ${folder} && npx create-react-app ${sampleGenerateMessage.directory}`,
+    function (error, stdout, stderr) {
+      console.log("stdout: " + stdout);
+      console.log("stderr: " + stderr);
+      if (error !== null) {
+        console.log("exec error: " + error);
+      }
+      makeFolder();
+      dfs(sampleGenerateMessage.data);
+      vscode.window.showInformationMessage("Done?");
+    }
+  );
 }
