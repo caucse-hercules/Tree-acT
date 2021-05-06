eedca7012c6bbd64bb809f0cf07bdfee7cf8470a
Beopsik <tjswl756@naver.com>
2021-05-07 00:04:13 +0900

    feat:modify sampleData

--- src/common/sampleData.ts
+++ src/common/sampleData.ts
@@ -4,24 +4,138 @@ export const sampleData: TreeNode = {
   name: "App",
   children: [
     {
-      name: "TodoContainer",
+      name: "Header",
       children: [
         {
-          name: "AddTodo",
+          name: "Navigator",
           children: [
             {
-              name: "Worker",
+              name: "JuniorNaver",
+            },
+            {
+              name: "HappyBin",
+            }
+          ],
+        },
+        {
+          name: "SearchBar"
+        },
+      ],
+    },
+    {
+      name: "Content",
+      children: [
+        {
+          name: "TapMenu",
+          children: [
+            {
+              name: "Mail",
+              children: [
+                {
+                  name:"writeMail"
+                },
+                {
+                  name:"writeMailtoMe"
+                }
+              ]
+            },
+            {
+              name: "Cafe",
+            },
+            {
+              name: "Blog",
+            },
+            {
+              name: "Shopping",
+            },
+            {
+              name: "Pay",
+            },
+            {
+              name: "News",
             },
           ],
         },
         {
-          name: "TodoList",
+          name: "Login",
           children: [
             {
-              name: "TodoItem",
+              name: "Email",
+            },
+            {
+              name: "Password",
             },
           ],
         },
+        {
+          name: "NewsBoard",
+          children: [
+            {
+              name: "JTBC",
+            },
+            {
+              name: "KBS",
+              children: [
+                {
+                  name: "Article",
+                  children: [
+                    {
+                      name: "Advertise",
+                      children: [
+                        {
+                          name: "ADcontent",
+                          children: [
+                            {
+                              name: "ADImage",
+                            },
+                          ]
+                        },
+                      ]
+                    },
+                  ]
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "Footer",
+      children: [
+        {
+          name: "Creators",
+          children: [
+            {
+              name: "Creator",
+            },
+            {
+              name: "SmallBusiness",
+            },
+          ],
+        },
+        {
+          name: "Partners",
+          children: [
+            {
+              name: "SME",
+            },
+            {
+              name: "Store",
+            },
+          ],
+        },
+        {
+          name: "Developers",
+          children: [
+            {
+              name: "NaverDeveloperCenter",
+            },
+            {
+              name: "OpenAPI",
+            },
+          ],
+        }
       ],
     },
   ],
