21ed89da5257028e510e8970dd4581603f1a6132
Beopsik <tjswl756@naver.com>
2021-06-02 20:29:01 +0900

    feat: change sample json and makeComponent function

--- common/sampleData.ts
+++ common/sampleData.ts
@@ -4,6 +4,55 @@ export const initialData: NewTreeNode[] = [
   {
     id: 0,
     name: "App",
+    children: [1, 2, 3],
+    isExpanded: true,
+  },
+  {
+    id: 1,
+    name: "Header",
+    parent: 0,
+    children: [4, 7],
+    isExpanded: true,
+  },
+  {
+    id: 2,
+    name: "Content",
+    parent: 0,
+    children: [],
+    isExpanded: true,
+  },
+  {
+    id: 3,
+    name: "Footer",
+    parent: 0,
+    children: [],
+    isExpanded: true,
+  },
+  {
+    id: 4,
+    name: "Navigator",
+    parent: 1,
+    children: [5, 6],
+    isExpanded: true,
+  },
+  {
+    id: 5,
+    name: "JuniorNaver",
+    parent: 4,
+    children: [],
+    isExpanded: true,
+  },
+  {
+    id: 6,
+    name: "HappyBin",
+    parent: 4,
+    children: [],
+    isExpanded: true,
+  },
+  {
+    id: 7,
+    name: "SearchBar",
+    parent: 1,
     children: [],
     isExpanded: true,
   },
@@ -153,5 +202,5 @@ export const sampleData: TreeNode = {
 export const sampleGenerateMessage: MessageData = {
   command: "generateApp",
   directory: "sample-app",
-  data: sampleData,
+  data: initialData,
 };
