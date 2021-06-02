b85cabebf58c25dcaadbc8a7fc939c8182bc9247
imdigo <limdongyoung0@gmail.com>
2021-06-02 14:17:54 +0900

    fix: add first App component to init state

--- treeAct-extension/src/state.ts
+++ treeAct-extension/src/state.ts
@@ -1,4 +1,11 @@
 import { NewTreeNode } from "../../common/types";
 
 // eslint-disable-next-line prefer-const
-export let treeState: NewTreeNode[] = [];
+export let treeState: NewTreeNode[] = [
+  {
+    id: 0,
+    name: "App",
+    children: [],
+    isExpanded: true,
+  },
+];
