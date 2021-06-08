767c17d2b7dffa246ec243aa6c00b8e8ebd2fadb
박준수 <tony180322@gmail.com>
2021-06-08 22:43:55 +0900

    chore: add comments in test code

--- treeAct-extension/src/test/suite/extension.test.ts
+++ treeAct-extension/src/test/suite/extension.test.ts
@@ -6,30 +6,38 @@ import { testGenerateMessage } from "../../../../common/testData";
 import { NewTreeNode } from "../../../../common/types";
 
 suite("Extension d Test Suite", function () {
+  // Set timeout for waiting execute
   this.timeout(100000);
 
   test("Run Test", async () => {
+    // Path where test application is installed
     const testPath = "./src/test";
+
     const testMessage = testGenerateMessage;
     const data: any = testMessage.data;
 
+    // If test application already exists, remove it.
     if (fs.existsSync(testPath + "/" + testMessage.directory)) {
       rimraf.sync(testPath + "/" + testMessage.directory);
     }
 
     await run(testMessage, testPath);
 
+    // Get components' name from input data to test if test application is installed well
     const nameList = await getComponentName(data);
 
+    // Get components' name from installed test application to test if test application is installed well
     const createNameList = await getCreateComponentName(
       testPath + "/" + testMessage.directory + "/src"
     );
 
+    // Test
     const equal = checkComponent(nameList, createNameList);
     assert.strictEqual(equal, true);
   });
 });
 
+// Function for comparing input components' name and installed components' name
 const checkComponent = (inputNameList: string[], createNameList: string[]) => {
   inputNameList.sort((one, two) => (one > two ? -1 : 1));
   createNameList.sort((one, two) => (one > two ? -1 : 1));
@@ -43,6 +51,7 @@ const checkComponent = (inputNameList: string[], createNameList: string[]) => {
   return true;
 };
 
+// Function to get input component's name
 const getComponentName = async (data: NewTreeNode[]) => {
   const nameList: string[] = [];
 
@@ -53,6 +62,7 @@ const getComponentName = async (data: NewTreeNode[]) => {
   return nameList;
 };
 
+// Function to get installed components' name
 const getCreateComponentName = async (dirPath: string) => {
   const files = fs.readdirSync(dirPath + "/components");
   const sliceFiles: string[] = [];
